6f8eae3408666608271e29118af8770d6e27d7ce
AayushiAhlawat <ahlawataayushi@gmail.com>
2024-04-18 01:01:08 -0400

    Commit

--- app/src/androidTest/java/com/example/chatapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/chatapp/ExampleInstrumentedTest.java
@@ -22,5 +22,10 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.chatapp", appContext.getPackageName());
+   
     }
+
+
 }
+
+
